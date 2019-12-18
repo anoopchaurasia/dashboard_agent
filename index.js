c54d23a24d5471f33ae999ad1186ede4a47a191b
anoopchaurasia <anoop@vumonic.com>
2019-12-18 12:51:14 +0530

    add ip

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 'use strict';
 let config = require("./.config.json");
 async function interval_function(){
-    let ec2metadata = runCommand("ec2metadata");
-    let server_name = config.name +"->"+ ec2metadata.split("\n").filter(x=>x.match(/local-ipv4/))[0].split(":")[1];
+        console.log("dfdf")
+    let ec2metadata = runCommand({value: "ec2metadata"}).ec2metadata.split("\n");
+    let server_name = config.name +"->"+ ec2metadata.filter(x=>x.match(/local-ipv4/))[0].split(":")[1];
     config.syscommands.forEach(async x=>{
         let data = runCommand(x);
         senddata({data, server_name, command_name: x.name, formatter: x.formatter});
     });
 
-    senddata({data: runCommand("ec2metadata"), server_name, command_name: config.name, formatter: "ec2metadata"});
+    senddata({data: ec2metadata, server_name, command_name: config.name, formatter: "ec2metadata"});
 }
 
 function runCommand(command){
@@ -18,8 +19,9 @@ function runCommand(command){
 
 function senddata(message){
     const dgram = require('dgram');
+console.log(message.command_name)
     message = Buffer.from( JSON.stringify(message));
-    const client = dgram.createSocket('udp4');
+        const client = dgram.createSocket('udp4');
     client.send(message, 41234, config.host||"localhost", (err) => {
         client.close();
     });
