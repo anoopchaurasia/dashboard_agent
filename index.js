48e4882a0a890f0844059928af12c7bfec92d6a6
anoopchaurasia <anoop@vumonic.com>
2019-12-18 12:28:52 +0530

    add ip

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
 let config = require("./.config.json");
 async function interval_function(){
+    let ec2metadata = runCommand("ec2metadata");
+    let server_name = config.name +"->"+ ec2metadata.split("\n").filter(x=>x.match(/local-ipv4/))[0].split(":")[1];
     config.syscommands.forEach(async x=>{
         let data = runCommand(x);
-        senddata({data, server_name:config.name, command_name: x.name, formatter: x.formatter});
+        senddata({data, server_name, command_name: x.name, formatter: x.formatter});
     });
 
-    senddata({data: runCommand("ec2metadata"), server_name:config.name, command_name: config.name, formatter: "ec2metadata"});
+    senddata({data: runCommand("ec2metadata"), server_name, command_name: config.name, formatter: "ec2metadata"});
 }
 
 function runCommand(command){
